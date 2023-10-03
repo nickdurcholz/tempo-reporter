303d122764cd1dad1c818c660f71504df50a684f
Nick Durcholz <nick.durcholz@pm.me>
2023-10-03 12:30:03 -0400

    feat: initial version of import command

--- src/tempo-reporter/ImportCommand.cs
+++ src/tempo-reporter/ImportCommand.cs
@@ -3,7 +3,6 @@ using System.Globalization;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
-using System.Text.Encodings.Web;
 using CliFx.Infrastructure;
 using CsvHelper;
 using TimeSpanParserUtil;
@@ -14,8 +13,6 @@ public class ImportCommand : ICommand
 {
     public static readonly HttpClient Client = new();
     private static readonly Uri TempoBaseUri = new("https://api.tempo.io/4/");
-    private IConsole? _console;
-    private Uri? _jiraBaseUri;
 
     [CommandOption(
         "file",
@@ -55,101 +52,46 @@ public class ImportCommand : ICommand
 
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        _console = console;
-        _jiraBaseUri = new Uri($"https://{JiraDomain}/rest/api/2/");
+        var jiraBaseUri = new Uri($"https://{JiraDomain}/rest/api/2/");
         ArgumentException.ThrowIfNullOrEmpty(TempoApiToken);
         ArgumentException.ThrowIfNullOrEmpty(JiraApiToken);
         ArgumentException.ThrowIfNullOrEmpty(JiraUser);
 
-        var input = string.IsNullOrEmpty(File)
-            ? console.Input
-            : new StreamReader(new FileStream(File, FileMode.Open, FileAccess.Read, FileShare.Read));
+        var input = OpenInputFile(console);
         var data = ParseCsv(input);
 
-        var issueIdMap = await GetIssueIdMap(data);
         await foreach (var worklog in GetWorklogs(data.Select(d => d.Date)))
-            await DeleteWorklog(worklog);
+            await DeleteWorklog(worklog, console);
+
+        var startTimes = new Dictionary<DateOnly, TimeOnly>();
+        foreach (var row in data)
+            await CreateWorklog(row, startTimes, jiraBaseUri, console);
 
         if (input != console.Input)
             input.Dispose();
     }
 
-    private async Task DeleteWorklog(TempoWorklogsResult.WorklogResult worklog)
+    private StreamReader OpenInputFile(IConsole console)
     {
-        var time = TimeSpan.FromSeconds(worklog.TimeSpentSeconds);
-        var timeSpent = Math.Truncate(time.TotalHours) > 0.0
-            ? $"{Math.Truncate(time.TotalHours):0}h {time.Minutes}m"
-            : $"{time.Minutes}m";
-        
-        _console!.Output.WriteLine($"Deleting worklog {worklog.TempoWorklogId} for {timeSpent} on {worklog.StartDate} for issue {worklog.Issue?.Id}");
-        var request = MakeTempoRequest(HttpMethod.Delete, $"worklogs/{worklog.TempoWorklogId}");
-        var response = await Client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-    }
-
-    private async IAsyncEnumerable<TempoWorklogsResult.WorklogResult> GetWorklogs(IEnumerable<DateOnly> dates)
-    {
-        const int pageSize = 500;
-        foreach (var date in dates.Distinct())
+        StreamReader input;
+        if (string.IsNullOrEmpty(File))
         {
-            var request = MakeTempoRequest(HttpMethod.Get, $"worklogs?from={date:yyyy-MM-dd}&to={date:yyyy-MM-dd}");
-            var response = await Client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-
-            var result = await response.Content.ReadFromJsonAsync<TempoWorklogsResult>() ??
-                         throw new InvalidOperationException($"{request.RequestUri} returned a null response");
-
-            if (result.Metadata!.Count > pageSize)
-                throw new InvalidOperationException(
-                    $"{request.RequestUri} returned {result.Metadata.Count} result, which is more than the maximum: {pageSize}");
-
-            foreach (var r in result.Results!)
-                yield return r;
+            input = console.Input;
         }
-    }
-
-    private HttpRequestMessage MakeTempoRequest(HttpMethod httpMethod, string? relativeUri)
-    {
-        var request = new HttpRequestMessage
+        else
         {
-            Method = httpMethod,
-            RequestUri = new Uri(TempoBaseUri, relativeUri),
-            Headers =
+            if (File.StartsWith('~'))
             {
-                Authorization = new AuthenticationHeaderValue("Bearer", TempoApiToken),
-                Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
+                var homeDir = Environment.GetEnvironmentVariable("HOME") ??
+                              Environment.GetEnvironmentVariable("USERPROFILE") ??
+                              throw new InvalidOperationException($"Unable to find home dir. Cannot expand ~ in '{File}'");
+                File = Path.Combine(homeDir, File.Substring(1).TrimStart('/', '\\'));
             }
-        };
-        return request;
-    }
 
-    private async Task<IDictionary<string, string>> GetIssueIdMap(IEnumerable<CsvRow> rows)
-    {
-        const int pageSize = 500;
-        var keys = rows.Select(r => r.IssueKey).Distinct();
-        var keysJql = UrlEncoder.Default.Encode($"key in ({string.Join(", ", keys)}");
-        Debug.Assert(_jiraBaseUri != null, nameof(_jiraBaseUri) + " != null");
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri(_jiraBaseUri, $"search?maxResults={pageSize}&fields=id,key&jql={keysJql}"),
-            Headers =
-            {
-                Authorization = new AuthenticationHeaderValue(
-                    "Basic",
-                    Convert.ToBase64String(Encoding.UTF8.GetBytes($"{JiraUser}:{JiraApiToken}"))),
-                Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
-            }
-        };
-        var response = await Client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadFromJsonAsync<IssueSearchResult>() ??
-                     throw new InvalidOperationException("Jira issue search returned a null response");
-        if (result.Total > pageSize)
-            throw new CommandException($"The jira search returned more than the maximum allowed results: {pageSize}");
-        if (result.Issues == null)
-            throw new CommandException("The jira search returned no issues");
-        return result.Issues.ToDictionary(i => i.Key, i => i.Id);
+            input = new StreamReader(new FileStream(File, FileMode.Open, FileAccess.Read, FileShare.Read));
+        }
+
+        return input;
     }
 
     private static List<CsvRow> ParseCsv(StreamReader input)
@@ -203,25 +145,100 @@ public class ImportCommand : ICommand
         return data;
     }
 
+    private async IAsyncEnumerable<TempoWorklogsResult.WorklogResult> GetWorklogs(IEnumerable<DateOnly> dates)
+    {
+        const int pageSize = 500;
+        foreach (var date in dates.Distinct())
+        {
+            var request = MakeTempoRequest(HttpMethod.Get, $"worklogs?from={date:yyyy-MM-dd}&to={date:yyyy-MM-dd}");
+            var response = await Client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<TempoWorklogsResult>() ??
+                         throw new InvalidOperationException($"{request.RequestUri} returned a null response");
+
+            if (result.Metadata!.Count > pageSize)
+                throw new InvalidOperationException(
+                    $"{request.RequestUri} returned {result.Metadata.Count} result, which is more than the maximum: {pageSize}");
+
+            foreach (var r in result.Results!)
+                yield return r;
+        }
+    }
+
+    private async Task DeleteWorklog(TempoWorklogsResult.WorklogResult worklog, IConsole console)
+    {
+        var time = TimeSpan.FromSeconds(worklog.TimeSpentSeconds);
+        var timeSpent = Math.Truncate(time.TotalHours) > 0.0
+            ? $"{Math.Truncate(time.TotalHours):0}h {time.Minutes}m"
+            : $"{time.Minutes}m";
+        
+        console.Output.WriteLine($"Deleting worklog {worklog.TempoWorklogId} for {timeSpent} on {worklog.StartDate} for issue {worklog.Issue?.Id}");
+        var request = MakeTempoRequest(HttpMethod.Delete, $"worklogs/{worklog.TempoWorklogId}");
+        var response = await Client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+    }
+
+    private async Task CreateWorklog(
+        CsvRow row,
+        Dictionary<DateOnly, TimeOnly> startTimes,
+        Uri jiraBaseUri,
+        IConsole console)
+    {
+        var time = row.Time;
+        var timeSpent = Math.Truncate(time.TotalHours) > 0.0
+            ? $"{Math.Truncate(time.TotalHours):0}h {time.Minutes}m"
+            : $"{time.Minutes}m";
+        console.Output.WriteLine($"Creating worklog for {timeSpent} on {row.Date:yyyy-MM-dd} for issue {row.IssueKey}");
+        
+        if (startTimes.TryGetValue(row.Date, out var startTime))
+            startTime = new TimeOnly(8, 0);
+        startTimes[row.Date] = startTime.Add(time);
+
+        var worklogDateTime = row.Date.ToDateTime(startTime, DateTimeKind.Local).ToUniversalTime();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(jiraBaseUri, $"issue/{row.IssueKey}/worklog?adjustEstimate=leave&notifyUsers=true"),
+            Headers =
+            {
+                Authorization = new AuthenticationHeaderValue(
+                    "Basic",
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes($"{JiraUser}:{JiraApiToken}"))),
+                Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
+            },
+            Content = JsonContent.Create(new
+            {
+                comment = row.Description ?? $"Working on issue {row.IssueKey}",
+                started = $"{worklogDateTime:yyyy-MM-ddTHH:mm:ss.fff}+0000",
+                timeSpentSeconds = (int)time.TotalSeconds
+            })
+        };
+        var response = await Client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+    }
+
+    private HttpRequestMessage MakeTempoRequest(HttpMethod httpMethod, string? relativeUri)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = httpMethod,
+            RequestUri = new Uri(TempoBaseUri, relativeUri),
+            Headers =
+            {
+                Authorization = new AuthenticationHeaderValue("Bearer", TempoApiToken),
+                Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
+            }
+        };
+        return request;
+    }
+
     private class CsvRow
     {
         public DateOnly Date { get; set; }
         public TimeSpan Time { get; set; }
         public string IssueKey { get; set; } = "";
         public string? Description { get; set; }
-    }
-}
-
-public class IssueSearchResult
-{
-    //https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-search/#api-rest-api-3-search-get
-    public int Total { get; set; }
-    public Issue[]? Issues { get; set; }
-
-    public class Issue
-    {
-        public string Id { get; set; } = "";
-        public string Key { get; set; } = "";
     }
 }
 
